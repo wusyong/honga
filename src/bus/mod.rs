941e7e1a746051c262e778f1bd5e245239ca470d
Ngo Iok Ui <wusyong9104@gmail.com>
2020-11-10 21:44:47 +0800

    Introduce CLINT & PLIC

--- src/bus/mod.rs
+++ src/bus/mod.rs
@@ -1,11 +1,17 @@
 //! System bus contains memory & memory-mapped peripheral devices.
 
+mod clint;
 mod memory;
+mod plic;
 
+pub use clint::{CLINT_BASE, CLINT_SIZE};
 pub use memory::{MEMORY_BASE, MEMORY_SIZE};
+pub use plic::{PLIC_BASE, PLIC_SIZE};
 
 use crate::exception::Exception;
+use clint::Clint;
 use memory::Memory;
+use plic::Plic;
 
 trait Device {
     fn load(&self, addr: u64, size: usize) -> Result<u64, Exception>;
@@ -14,17 +20,27 @@ trait Device {
 
 /// System bus.
 pub struct Bus {
+    clint: Clint,
     memory: Memory,
+    plic: Plic,
 }
 
 impl Bus {
     pub fn new(binary: Vec<u8>) -> Bus {
         Self {
             memory: Memory::new(binary),
+            clint: Clint::new(),
+            plic: Plic::new(),
         }
     }
 
     pub fn load(&self, addr: u64, size: usize) -> Result<u64, Exception> {
+        if CLINT_BASE <= addr && addr < CLINT_BASE + CLINT_SIZE {
+            return self.clint.load(addr, size);
+        }
+        if PLIC_BASE <= addr && addr < PLIC_BASE + PLIC_SIZE {
+            return self.plic.load(addr, size);
+        }
         if MEMORY_BASE <= addr {
             return self.memory.load(addr, size);
         }
@@ -32,6 +48,12 @@ impl Bus {
     }
 
     pub fn store(&mut self, addr: u64, size: usize, value: u64) -> Result<(), Exception> {
+        if CLINT_BASE <= addr && addr < CLINT_BASE + CLINT_SIZE {
+            return self.clint.store(addr, size, value);
+        }
+        if PLIC_BASE <= addr && addr < PLIC_BASE + PLIC_SIZE {
+            return self.plic.store(addr, size, value);
+        }
         if MEMORY_BASE <= addr {
             return self.memory.store(addr, size, value);
         }
